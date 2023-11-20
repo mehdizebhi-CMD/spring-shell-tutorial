5cc41fa0872080b17c701d2f6650c13343317225
mehdizebhi <mehdizebhi@yahoo.com>
2023-11-20 21:32:30 +0330

    Create WebGetCLI for return source of web URL in CLI.

--- src/main/java/dev/mehdizebhi/springshell/commands/webget/WebGetCLI.java
+++ src/main/java/dev/mehdizebhi/springshell/commands/webget/WebGetCLI.java
@@ -1,2 +1,26 @@
-package dev.mehdizebhi.springshell.commands.webget;public class WebGetCLI {
+package dev.mehdizebhi.springshell.commands.webget;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+@ShellComponent
+public class WebGetCLI {
+
+    // ----------------------------------------------------
+    // Command Line Interface Methods
+    // ----------------------------------------------------
+
+    @ShellMethod(key = {"wget"}, value = "Return the source content of a URL.")
+    public String webGetSource() {
+        return null;
+    }
+
+    @ShellMethod(key = {"wsave"}, value = "Save the source content of a URL to the file.")
+    public String webSaveSource() {
+        return null;
+    }
+
+    // ----------------------------------------------------
+    // Private Helper
+    // ----------------------------------------------------
 }
